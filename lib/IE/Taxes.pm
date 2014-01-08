c0679e2268d2065f5e92cf34a4d9e2b9c7e4126e
Alexander Yagovdik <yagovdik@gmail.com>
2014-01-09 00:12:34 +0300

    Attach database, sample db usage

--- lib/IE/Taxes.pm
+++ lib/IE/Taxes.pm
@@ -1,10 +1,20 @@
 package IE::Taxes;
 use Dancer ':syntax';
 
+use Plugin::Model;
+
+use Data::Dumper;
+
 our $VERSION = '0.1';
 
 get '/' => sub {
-    template 'index';
+
+	my @currencies = model('Currency')->all();
+	for my $currency ( @currencies ) {
+		my %data = $currency->get_inflated_columns();
+		print Dumper \%data;
+	}
+    # template 'index';
 };
 
 true;
