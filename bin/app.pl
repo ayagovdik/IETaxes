b581f0d2aad3d0af29cc02a6f99fb7c8375d7680
Alexander Yagovdik <yagovdik@gmail.com>
2014-01-08 23:06:24 +0300

    Dancer 1.3114

---
+++ bin/app.pl
@@ -0,0 +1,4 @@
+#!/usr/bin/env perl
+use Dancer;
+use IE::Taxes;
+dance;
---
+++ lib/IE/Taxes.pm
@@ -0,0 +1,10 @@
+package IE::Taxes;
+use Dancer ':syntax';
+
+our $VERSION = '0.1';
+
+get '/' => sub {
+    template 'index';
+};
+
+true;
